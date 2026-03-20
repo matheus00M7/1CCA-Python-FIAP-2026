e13d4489694bfb6c4ea43eb39c77e99692e722ca
matheus00M7 <m7developer01@gmail.com>
2026-03-20 11:32:25 -0300

    aula 2 variaveis e operadores

---
+++ aula02-variaveis-operadores/aula02.py
@@ -0,0 +1,22 @@
+print ("Oi matheus")
+
+print ("7" + "4")
+print ("7" + "4") # concatenaçao de strings
+
+'''
+comentarios
+de 
+multiplas   
+linhas
+'''
+
+#variaveis
+nome = ("matheus") #string
+idade = 26 # inteiro
+peso = 75.8 # float
+
+print (nome, idade, peso)
+print (f"oiiii {nome}!!!!")
+nome = input("dogote seu nome: ")
+idade = input("digite seu nome: ")
+
---
+++ aula02-variaveis-operadores/desafio1.py
@@ -0,0 +1,9 @@
+nome = input("digite seu nome: ")
+print(f"bem-vindo {nome}!!!!!")
+
+def data_de_nacimento():
+    dia = input("digite o dia do seu nacimento: ")
+    mes = input("digite o mes do seu nacimento: ")
+    ano = input("digite o ano do seu nacimento: ")
+
+    return f"{dia}/{mes}/ano"
